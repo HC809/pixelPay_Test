1498eb641faeb66323b8a29469652e394f7f910d
Hector Caballero <caballero.22.809@gmail.com>
2020-05-17 15:28:45 -0600

    tabla tarea migracion (foreign-key estado_id)

---
+++ app/Models/Tarea.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tarea extends Model
+{
+     //definicion de la tabla 'Estados'
+     protected $table = 'tareas';
+}
